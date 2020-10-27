3472f725320dd3199d2ecc34e1969d6c62ed5c03
ayaanqui <moahammedayaan.dev@gmail.com>
2020-10-27 16:58:45 -0500

    Added port argument, callback, and socket close

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
@@ -9,40 +10,43 @@ import java.lang.Thread;
 
 public class Server {
     int count = 1;
+    int port = 5555;
     ArrayList<ClientThread> clients = new ArrayList<ClientThread>();
     ServerThread server;
     private Consumer<Serializable> callback;
 
-    public Server(Consumer<Serializable> call) {
+    public Server(Consumer<Serializable> call, int port) {
         this.callback = call;
+        this.port = port;
         server = new ServerThread();
         server.start();
     }
 
-    public static void main(String[] args) {
-        Server s = new Server(null);
-    }
-
     class ServerThread extends Thread {
+        ServerSocket socket;
+
         public void run() {
-            int port = 5555;
             try {
-                ServerSocket socket = new ServerSocket(port);
+                this.socket = new ServerSocket(port);
 
                 System.out.println("Server is waiting for a client!");
 
                 while (true) {
-                    ClientThread client = new ClientThread(socket.accept(), count);
+                    ClientThread client = new ClientThread(this.socket.accept(), count);
                     System.out.println("New client connected...");
                     clients.add(client);
                     client.start();
-                    // callback.accept("Client #" + count + " added to pool");
+                    callback.accept("Client #" + count + " added to pool");
 
                     count++;
                 }
             } catch (Exception e) {
-                // callback.accept("Server socket did not launch");
+                callback.accept("Server socket did not launch");
             }
+        }
+
+        public void close() throws IOException {
+            this.socket.close();
         }
     }
 
@@ -80,7 +84,7 @@ public class Server {
             while (true) {
                 try {
                     String req = (String) in.readObject();
-                    // callback.accept("client: " + this.count + " sent: " + req);
+                    callback.accept("client: " + this.count + " sent: " + req);
                     this.out.writeObject("Message recieved: " + req);
                     updateClients("client #" + this.count + " said: " + req);
                 } catch (Exception e) {
