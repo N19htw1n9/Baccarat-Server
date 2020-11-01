b8e6974d91bc3e7f34b4476cc82cdff91def4d02
ayaanqui <moahammedayaan.dev@gmail.com>
2020-10-31 21:20:39 -0500

    Added getters/setters and equals methods

--- src/main/java/Server/BaccaratGame/Card.java
+++ src/main/java/Server/BaccaratGame/Card.java
@@ -8,4 +8,29 @@ public class Card {
         this.suite = suite;
         this.value = value;
     }
+
+    public String getSuite() {
+        return suite;
+    }
+
+    public void setSuite(String suite) {
+        this.suite = suite;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (((Card) obj).getSuite().equals(this.suite) && ((Card) obj).getValue() == this.value);
+    }
+
+    public String toString() {
+        return this.value + " of " + this.suite;
+    }
 }
