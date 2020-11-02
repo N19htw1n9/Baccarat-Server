57a8bfc109fdbe36d4c0be6fd01803edb519ae1b
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-11-01 20:25:31 -0600

    Initialized members

--- src/main/java/BaccaratGame/BaccaratGame.java
+++ src/main/java/BaccaratGame/BaccaratGame.java
@@ -8,12 +8,18 @@ public class BaccaratGame {
     private BaccaratDealer theDealer;
     private double currentBet;
     private double totalWinnings;
+    private String hand;
 
-    public BaccaratGame() {
-        theDealer = new BaccaratDealer();
+    public BaccaratGame(double currentBet, String hand) {
+        this.theDealer = new BaccaratDealer();
+        this.theDealer.generateDeck();
+        this.playerHand = this.theDealer.dealHand();
+        this.bankerHand = this.theDealer.dealHand();
+        this.currentBet = currentBet;
     }
 
     public double evaluateWinnings() {
+        String winner = BaccaratGameLogic.whoWon(this.bankerHand, this.playerHand);
         return 0.0;
     }
 }
