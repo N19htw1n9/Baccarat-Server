4c583e530bb790152e7b88bafe11e645a52a0358
Ayaan Siddiqui <moahammedayaan.dev@gmail.com>
2020-11-02 12:51:10 -0600

    Added default serialVersionUID

--- src/main/java/BaccaratGame/BaccaratInfo.java
+++ src/main/java/BaccaratGame/BaccaratInfo.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class BaccaratInfo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     public double bid;
     public String hand;
 
