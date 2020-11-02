85ed418e317cb8e095c078298fb2d23a325b2fc1
ayaanqui <moahammedayaan.dev@gmail.com>
2020-11-01 20:22:31 -0600

    Added new fields

--- src/main/java/BaccaratGame/BaccaratInfo.java
+++ src/main/java/BaccaratGame/BaccaratInfo.java
@@ -1,10 +1,18 @@
 package BaccaratGame;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class BaccaratInfo implements Serializable {
     public double bid;
     public String hand;
+
+    public ArrayList<String> playerHand; // ["1C", "12H"]
+    public ArrayList<String> bankerHand; // ["3D", "4S"]
+
+    public String winner;
+
+    public double winnings;
 
     public BaccaratInfo(double bid, String hand) {
         this.bid = bid;
